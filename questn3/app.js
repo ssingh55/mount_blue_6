6eed5644ed51c5f187b8813aabf2e5d8b9c974e1
Shubham Singh <shubham.singh@mountblue.io>
2018-07-05 12:08:42 +0530

    questn3 coding

---
+++ questn3/app.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	var myData = data;
+	var 
+    }
---
+++ questn3/extract.js
@@ -0,0 +1,33 @@
+// let teamList = require('../questn1/extract')
+let csvToJson = require('convert-csv-to-json');
+
+let json = csvToJson.fieldDelimiter(',').getJsonFromCsv("../ipl_data/matches.csv");
+var id = [];
+var teamPlaying = new Set();
+for (let i = 0, j = 0; i < json.length; i++) {
+    if (json[i].season == '2016') {
+        id[j] = json[i].id;
+        j++;
+        teamPlaying.add(json[i].team1);
+    }
+}
+teamPlaying = Array.from(teamPlaying);
+console.log(teamPlaying);
+
+var extraRuns = [];
+let json1 = csvToJson.fieldDelimiter(',').getJsonFromCsv("../ipl_data/deliveries.csv");
+var extra, extra1, team1;
+for (var teams of teamPlaying) {
+    extra = 0;
+    for (let j = 0; j < id.length; j++) {
+        for (let i = 0; i < json1.length; i++) {
+            // console.log(json1[i].id);
+            if (json1[i].match_id == id[j]) {
+                if (teams == json1[i].bowling_team) {
+                    extra = extra + Number(json1[i].extra_runs);
+                }
+            }
+        }
+    }
+                console.log(extra);
+}
