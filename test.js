d9464cb633be6e9aea0802d12c1481fac8bf862f
ssingh55 <suvamsingh55@gmail.com>
2018-07-09 08:32:33 +0530

    with test nearlydone

--- test.js
+++ test.js
@@ -1,13 +1,27 @@
 var expect = require('chai').expect;
 var test = require('./testquestn1.js');
 
-describe('get total match per year',function(){
-	it('getting the number of matches ',function(){
-		let expectedResult= {
-			2008: 5,
-			2008:12,
-			2009:13
-		}
-		expect(test.getMatchesPerYear().equals(expectedResult))
-	})
+describe('get total match per year', function() {
+    it('getting the number of matches ', function() {
+        let expectedResult = { 
+        	'2008': 58,
+        	'2009': 57, 
+        	'2010': 60, 
+        	'2011': 73, 
+        	'2012': 74, 
+        	'2013': 76, 
+        	'2014': 60, 
+        	'2015': 59, 
+        	'2016': 60, 
+        	'2017': 59 
+        }
+        expect(test.getMatchesPerYear(1).then(function(data){
+        	try{
+        	expect(data).to.deep.equal(expectedResult)
+        	// done();
+        } catch(e){
+        	// done(e)
+        }
+        })
+        	
 })
--- testquestn1.js
+++ testquestn1.js
@@ -6,42 +6,48 @@ let getMatchesPerYear = (matchData) => {
     return new Promise(function(resolve, reject) {
         var totalMatchPerYear = [];
 
-        fs.createReadStream('./ipl_data/matches.csv').pipe(csv()).on('data', function(data) {
+        fs.createReadStream('./ipl_data/matches.csv').pipe(csv()).on('data', function(data, err) {
 
-                
-                    if (Number(data[1])) {
+            if (err) {
+                reject(err);
+            } else {
+                if (Number(data[1])) {
 
-                        totalMatchPerYear.push(data[1]);
-
-
-                }
-            }).on('end', function() {
-
-                var map = totalMatchPerYear.reduce(noOfMatchesPerYear, {});
-
-                function noOfMatchesPerYear(counter, year) {
-
-                    counter[year] = ++counter[year] || 1;
-                    return counter;
-
+                    totalMatchPerYear.push(data[1]);
                 }
 
-                // console.log(JSON.stringify(map));
-                // console.log(map);
-                resolve(map)
-            });
+            }
 
-        }).catch(function(e){
+
+        }).on('end', function() {
+
+            var map = totalMatchPerYear.reduce(noOfMatchesPerYear, {});
+
+            function noOfMatchesPerYear(counter, year) {
+
+                counter[year] = ++counter[year] || 1;
+                return counter;
+
+            }
+
+            // console.log(JSON.stringify(map));
+            // console.log(map);
+            resolve(map)
+        });
+
+    }).catch(function(e) {
 
     })
+}
+getMatchesPerYear(1).then(function(data) {
+    try {
+        console.log(data);
+        done();
+    } catch (e) {
+        done(e);
     }
-getMatchesPerYear(1).then(function(data){
-	try{
-		console.log(data);
-		done();
-	} catch (e){
-		done(e);
-	}
+}).catch(function(e){
+	
 });
 module.exports = {
     getMatchesPerYear: getMatchesPerYear
